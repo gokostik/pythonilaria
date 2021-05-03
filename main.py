6c3ebafa72aa3444f2c8230e49cdf8fad538e3e0
uapc <gokostik@ukr.net>
2021-05-03 20:33:34 +0300

    888

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 print("Я, Іларія-Болгарія")
-name = input("а як тебе звуть?")
-print(f"Привіт гавнюк,  {name}")
+math = True
+is_a_cool = False
+is_b_cool = False
+
+#name = input("а як тебе звуть?")
+#print(f"Привіт гавнюк,  {name}")
+while math:
+   while not is_a_cool:
+        try:
+            a = float(input("введіть а=").replace(",","."))
+            is_a_cool = True
+        except:
+            print("а має бути числом")
+
+
+   while not is_b_cool:
+        try:
+            b = float(input("введіть b=").replace(",","."))
+            is_b_cool = True
+        except:
+            print("b має бути числом")
+
+   is_a_cool = False
+   is_b_cool = False
+
+   print(f"a-b={a-b}")
+   print(f"a+b={a+b}")
+   print(f"a*b={a*b}")
+   try:
+       print(f"a/b={a/b}")
+   except:
+       print("a/b= Ділити на нуль не можна")
